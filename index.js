54c879503da88ee358aa8e3f8f80d55619231e22
Amir Mohamad Eftekhari <57019905+amirrrrrrr7@users.noreply.github.com>
2021-07-17 12:12:03 +0430

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+
+class Counter extends React.Component {
+
+    state ={
+        value : 0,
+
+    }
+
+    counter = ()=> {
+
+        return React.createElement('span',{},this.state.value)
+    }
+
+    increaseButton = ()=>{
+        return React.createElement(
+            'button',
+            {
+                style:{margin:'10px'},
+                onClick :()=>{ this.setState({value: this.state.value +1})}
+            },
+            'Increase'
+            )
+    }
+
+    decreaseButton =()=>{
+        return React.createElement('button',
+            {
+                onClick :()=>{ this.setState({value: this.state.value -1})}
+            },
+            'Decrease'
+        )
+    }
+
+    render(){
+        return React.createElement(
+            'div',
+            {},
+            this.counter(),
+            this.increaseButton(),
+            this.decreaseButton()
+        )
+    }
+}
+
+ReactDOM.render(React.createElement(Counter) , document.querySelector('#root'))
